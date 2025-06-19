54bb86c34d906abb04287d78a369d2b0e1a1182d
CodingBeagle <kasper.rie@gmail.com>
2025-06-19 20:03:53 +0200

    moved cube mesh generation to separate file

---
+++ CubeMesh.h
@@ -0,0 +1,9 @@
+#ifndef CUBE_MESH_H
+#define CUBE_MESH_H
+
+#include <raylib.h>
+
+// Generate a cube mesh with vertex positions and normals for flat shading
+Mesh GenerateCubeMesh();
+
+#endif // CUBE_MESH_H
